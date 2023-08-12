152be8d7a2e921859ae24e95a1bdfbeac9dd0ab4
hectorsestanley <62483627+hectorsestanley@users.noreply.github.com>
2023-08-12 08:32:30 +0100

    Update springapp_mvp1.py

--- springapp_mvp1.py
+++ springapp_mvp1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+#from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
 
 # Streamlit app starts here
